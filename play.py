15386288192fef7add29100c04a7414532a0b66f
Sakura_Li <me@nvme0n1p.dev>
2023-10-13 12:43:54 +0800

    feat: Rebuild

--- play.py
+++ play.py
@@ -5,22 +5,16 @@ import time
 from gym_2048.envs import Game2048Env
 
 
-# def make_model():
-#     ipt = tf.keras.layers.Input(shape=(4, 4, 1))
-#     x = tf.keras.layers.Conv2D(256, 2, activation="relu")(ipt)
-#     x = tf.keras.layers.Conv2D(256, 2, activation="relu")(x)
-#     x = tf.keras.layers.Flatten()(x)
-#     x = tf.keras.layers.Dense(256, activation="relu")(x)
-#     actions = tf.keras.layers.Dense(4, activation="linear")(x)
-#     model = tf.keras.Model(inputs=ipt, outputs=actions)
-#     return model
+# 设置TensorFlow日志级别
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 
 # model = make_model()
-model = tf.keras.models.load_model("models/dqn.h5")
-model.load_weights("models/dqn_weights_best.pkl")
+model = tf.keras.models.load_model("model.keras")
 
-print(model.summary())
+model.load_weights("model.pkl")
+
+# print(model.summary())
 
 env = Game2048Env()
 
@@ -28,18 +22,15 @@ env.reset()
 
 # env.render(mode="human")
 while True:
+    os.system("cls")
     env.render(mode="human")
     state = np.array(env.get_board()).reshape(-1, 4, 4, 1)
-    # action = np.argmax(model(state)[0])
-    action = 0
-    if np.random.rand() > 0.9:
-        action = np.argmax(model(state)[0])
-
-    else:
+    if np.random.rand() <= 0.1:
         action = np.random.randint(0, 4)
+    else:
+        action = np.argmax(model.predict(state)[0])
     env.step(action)
     time.sleep(0.5)
-
     if env.isend():
-        exit(0)
-    os.system("cls")
+        print("Game Over!")
+        break
--- train.py
+++ train.py
@@ -1,151 +1,135 @@
+import os
+
+# 设置TensorFlow日志级别
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+import logging
 import tensorflow as tf
-import random
-import sys
-import time
-from collections import deque
-
-# from rl.agents import DQNAgent
-# from rl.memory import SequentialMemory
-# from rl.policy import BoltzmannQPolicy
-from keras.optimizers import adam_v2
 import numpy as np
-
+from tensorflow.keras.layers import Conv2D, Flatten, Dense
+from tensorflow.keras import Model, Input
+from tensorflow.keras.optimizers import Adam
+from collections import deque
+import random
 from gym_2048.envs import Game2048Env
+import sys
 
+tf.get_logger().setLevel("FATAL")
+# Functions
+
+
+def convert_to_batch(batch):
+    states = np.array([i[0] for i in batch]).reshape(-1, 4, 4, 1)
+    actions = np.array([i[1] for i in batch])
+    rewards = np.array([i[2] for i in batch])
+    next_states = np.array([i[3] for i in batch]).reshape(-1, 4, 4, 1)
+    return states, actions, rewards, next_states
+
+
+def save_model():
+    model.save("model.keras")
+    model.save_weights("model.pkl")
+    print("Saved model")
+    return
+
+
+# Hyperparameters
+BATCH_SIZE = 64  # size of data used to experience replay
+GAMMA = 0.99  # discount factor
+EPSILON = 1.0  # exploration rate
+EPSILON_MIN = 0.1  # minimum exploration rate
+EPSILON_DECAY = 0.999  # decay rate of exploration rate
+TARGET_UPDATE = 100  # update target network every 5 episodes
+MEMORY_SIZE = 50000  # size of experience replay memory
+LEARNING_RATE = 0.001  # learning rate
+EPOCHES = 1000  # number of training epochs
+RESET_EPSILON_TIMES = 10  # reset epsilon for 10 times.
+# Environment
 env = Game2048Env()
+memory = deque(maxlen=MEMORY_SIZE)
 
 
+# Model
 def make_model():
-    ipt = tf.keras.layers.Input(shape=(4, 4, 1))
-    # x = tf.keras.layers.Conv2D(32, 3, activation="relu")(ipt)
-    # x = tf.keras.layers.Conv2D(64, 1, activation="relu")(x)
-    # # x = tf.keras.layers.Conv2D(128, 2, activation="relu")(x)
-    # # x = tf.keras.layers.Conv2D(256, activation="relu")(ipt)
-    x = tf.keras.layers.Dense(256, activation="relu")(ipt)
-    x = tf.keras.layers.Dense(256, activation="relu")(x)
-    x = tf.keras.layers.Flatten()(x)
-    actions = tf.keras.layers.Dense(4, activation="linear")(x)
-    model = tf.keras.Model(inputs=ipt, outputs=actions)
+    inputs = tf.keras.Input(shape=(4, 4, 1))
+    x = Conv2D(32, (3, 3), padding="same", activation="relu")(inputs)
+    x = Conv2D(64, (3, 3), padding="same", activation="relu")(x)
+    x = Flatten()(x)
+    x = Dense(256, activation="relu")(x)
+    x = Dense(4, activation="linear")(x)
+    model = Model(inputs=inputs, outputs=x)
     return model
 
 
 model = make_model()
+target_model = make_model()
+
 try:
-    model.load_weights("models/dqn_weights_best.pkl")
-    print("Loaded Weights from dqn_weights_best.pkl")
+    model.load_weights("model.pkl")
+    print("Loaded model")
 except:
-    print("No Weights Found")
-    pass
-tmodel = make_model()
+    print("No model found")
 
-print(model.summary())
+target_model.set_weights(model.get_weights())
 
-optimizer = adam_v2.Adam()
-model.compile(optimizer, loss="mse")
-# tmodel.compile(optimizer, loss="mse")
-memory = deque(maxlen=5000)
+# Optimizer
+optimizer = Adam(learning_rate=LEARNING_RATE)
+model.compile(optimizer=optimizer, loss="mse")
 
-
-def convert_to_batch(samples):
-    states = np.array([sample[0] for sample in samples]).reshape(-1, 4, 4, 1)
-    actions = np.array([sample[1] for sample in samples])
-    rewards = np.array([sample[2] for sample in samples])
-    next_states = np.array([sample[3] for sample in samples]).reshape(-1, 4, 4, 1)
-    return states, actions, rewards, next_states
-
-
-def model_save(model, score):
-    print("Saving model...")
-    model.save("models/dqn.h5")
-    model.save_weights("models/dqn_weights_{}.pkl".format(int(score)))
-
-
-def train():
-    batch_size = 128
-    epochs = 1000
-    gamma = 0.95
-    epsilon = 1.0
-    global score
-    score = 0
-    print("Started Training")
-    tot_times = 0
-    env.reset()
-    try:
-        for i in range(epochs):  # we train for n rounds of game.
-            tot_times += 1
-            print("\nEpoch {}".format(i))
-            env.reset()
-            t = 0
-            times = time.time()
-            while True:
-                t += 1
-                sys.stdout.write(
-                    f"\r{t} steps played.Speed {1000*(time.time()-times)/t:.2f} ms/step. Score={env.score} .eps={epsilon:.2f} "
-                )
-                sys.stdout.flush()
-                state = np.array(env.get_board()).reshape(-1, 4, 4, 1)
-                if np.random.rand() < epsilon:  # random action
-                    action = np.random.randint(0, 4)
-                else:  # model action
-                    q_values = model.predict(state)[0]
-                    action = np.argmax(q_values)
-                # print(action)
-                next_state, reward, done, _ = env.step(action)
-                memory.append((state, action, reward, next_state, done))
-                batch = []
-                if len(memory) < batch_size:
-                    batch = memory
-                else:
-                    batch = random.sample(memory, batch_size)
-                states, actions, rewards, next_states = convert_to_batch(batch)
-                # print(states.shape, actions.shape, rewards.shape, next_states.shape)
-                next_q_values = tmodel.predict(next_states)
-                max_next_q_values = np.max(next_q_values, axis=1).reshape(-1, 1)
-                target_q_values = rewards + (1 - done) * gamma * max_next_q_values
-
-                with tf.GradientTape() as tape:
-                    q_values = model(states)
-                    action_q_values = tf.gather(q_values, actions, batch_dims=1)
-                    loss = tf.reduce_mean(tf.square(target_q_values - action_q_values))
-                    # print(
-                    #     "Step {},score={},Step={},max score={}".format(
-                    #         i, env.score, action, score
-                    #     )
-                    # )
-                    grads = tape.gradient(loss, model.trainable_variables)
-                    optimizer.apply_gradients(zip(grads, model.trainable_variables))
-
-                if env.isend():
-                    print(
-                        "\nEpoch {},Game End.Score {} max score={} max_tile={}, Status\n {}".format(
-                            i, env.score, score, env.max_tile, env.get_board()
-                        )
-                    )
-                    if score < env.score:
-                        # model.save("dqn_{}.h5".format(env.score))
-
-                        score = env.score
-                    break
-
-                if epsilon > 0.1:
-                    epsilon *= 0.999
-                if tot_times % 100 == 0:
-                    tmodel.set_weights(model.get_weights())  # update target model
-            if i % 5 == 0:
-                epsilon = 1.0  # reset epsilon
-
-    except Exception as e:
-        print(e)
-        print("Saving Model")
-        model.save("models/dqn.h5")
-        model.save_weights("models/dqn_weights_best.pkl")
-
+# Variables
+tot_times = 0
+# Training
 
 try:
-    train()
-    model_save(model, score)
-except KeyboardInterrupt as e:
-    print(e)
-    print("Saving Model")
-    model.save("models/dqn.h5")
-    model.save_weights("models/dqn_weights_best.pkl")
+    for epoch in range(EPOCHES):
+        env.reset()
+        t = 0
+        while True:
+            t += 1
+            tot_times += 1
+            sys.stdout.write(
+                f"\r\rEpoch: {epoch}. Steps: {t}. Exploration rate: {EPSILON:.2f}. Current Score: {env.score}. "
+            )
+            sys.stdout.flush()
+            state = np.array(env.get_board()).reshape(-1, 4, 4, 1)
+            if np.random.rand() <= EPSILON:
+                action = np.random.randint(0, 4)
+            else:
+                action = np.argmax(model.predict(state, verbose=0)[0])
+            next_state, reward, done, _ = env.step(action)
+            next_state = np.array(next_state).reshape(-1, 4, 4, 1)
+            memory.append((state, action, reward, next_state, done))
+            batch = []
+            if len(memory) >= BATCH_SIZE:
+                batch = random.sample(memory, BATCH_SIZE)
+            else:
+                batch = memory
+            states, actions, rewards, next_states = convert_to_batch(batch)
+            next_q_val = target_model.predict(next_states, verbose=0)
+            max_next_q_val = np.max(next_q_val, axis=1)
+            target_q_val = rewards + (1 - done) * GAMMA * max_next_q_val
+            with tf.GradientTape() as tape:
+                q_values = model(states)
+                actions_q_values = tf.gather(q_values, actions, batch_dims=1)
+                loss = tf.reduce_mean(tf.square(target_q_val - actions_q_values))
+
+            grads = tape.gradient(loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(grads, model.trainable_variables))
+            if env.isend():
+                break
+            if EPSILON > EPSILON_MIN:
+                EPSILON *= EPSILON_DECAY
+            if tot_times % TARGET_UPDATE == 0:  # update target network
+                target_model.set_weights(model.get_weights())
+        if (
+            epoch % RESET_EPSILON_TIMES == 0 and epoch <= 100
+        ):  # We use the first 0.1 epoches to explore with high probability.
+            EPSILON = 1.0  # RESET EPSILON
+        print(
+            f"\nGame over. Score: {env.score}. Steps: {t}. Total Trained steps: {tot_times}. \n End status:\n{env.get_board()}"
+        )
+        save_model()
+
+
+except:
+    print(f"\nInterupted. Total Trained steps: {tot_times}.")
+    save_model()
